7acc4534a2d7317d105fff16a47c17c5cd8fb658
qilezhang <zxzql2314@gmail.com>
2016-03-08 22:04:02 +0800

    第一次编译通过并成功运行

--- HeadFirst.DesignPatterns.Strategy/Duck.h
+++ HeadFirst.DesignPatterns.Strategy/Duck.h
@@ -15,15 +15,9 @@ namespace DesignPattern
 		Duck();
 		virtual ~Duck();
 
-<<<<<<< HEAD
-	public:
-		virtual void display() = 0;
-		void swin();
-=======
 	public :
 		virtual void display() = 0;
 		void swin();		
->>>>>>> 5ba178674029b0d825ebf3392091949b832fc993
 		void setFlyBehavior(FlyBehavior *fly);
 		void setQuackBehavior(QuackBehavior *quack);
 		void performFly();
